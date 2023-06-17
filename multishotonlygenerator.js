9d729db060d6a800b5707d1c05ac07499b0c8f67
x4080 <x4080@yahoo.com>
2023-06-18 05:49:37 +0700

    commit1

--- multishotonlygenerator.js
+++ multishotonlygenerator.js
@@ -106,13 +106,13 @@ order value is "pizza name, quantity" from input text`,
         input: `who is trump`,
         response: `{"message":"I only server pizza and nothing else","order":"","intent":"unknown","confirmed":"no"}`
     },
-//     {
-//         instruction: `message is respond from input text related to pizza order
-// intent value is unknown if not related to pizza
-// order value is "pizza name, quantity" from input text`,
-//         input: `Whats the weather`,
-//         response: `{"message":"I only server pizza and nothing else","order":"","intent":"unknown","confirmed":"no"}`
-//     },
+    //     {
+    //         instruction: `message is respond from input text related to pizza order
+    // intent value is unknown if not related to pizza
+    // order value is "pizza name, quantity" from input text`,
+    //         input: `Whats the weather`,
+    //         response: `{"message":"I only server pizza and nothing else","order":"","intent":"unknown","confirmed":"no"}`
+    //     },
     {
         instruction: `Here's a chat between pizza owner and customer`,
         input: ``,
@@ -124,20 +124,20 @@ Pizza Owner : Two pepperoni pizza coming up, would you like anything else ?
 Customer : No thats all
 Pizza Owner : Allright, good bye`
     },
-        {
-            instruction: `Here's a chat between pizza owner and customer`,
-            input: ``,
-            response: `Pizza Owner : Welcome to pizza Pipi, can I help you ?
+    {
+        instruction: `Here's a chat between pizza owner and customer`,
+        input: ``,
+        response: `Pizza Owner : Welcome to pizza Pipi, can I help you ?
     Customer : what is the weather like ?
     Pizza Owner : I only server pizza and nothing else`
-        },
-        {
-            instruction: `Here's a chat between pizza owner and customer`,
-            input: ``,
-            response: `Pizza Owner : Welcome to pizza Pipi, can I help you ?
-    Customer : Who is Trump ?
-    Pizza Owner : I only server pizza and nothing else`
-        },
+    },
+    // {
+    //     instruction: `Here's a chat between pizza owner and customer`,
+    //     input: ``,
+    //     response: `Pizza Owner : Welcome to pizza Pipi, can I help you ?
+    // Customer : Who is Trump ?
+    // Pizza Owner : I only server pizza and nothing else`
+    // },
 
 ]
 
