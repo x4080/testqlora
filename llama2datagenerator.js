64e98a9f6b72327754032de683f841f82e676f2a
x4080 <x4080@yahoo.com>
2023-09-10 05:28:21 +0700

    commit1

--- llama2datagenerator.js
+++ llama2datagenerator.js
@@ -1,62 +1,85 @@
 const filename = "llama2data.jsonl"
 const fs = require('fs');
 
+let instruction = `Choose from ('weather' or 'other topic') what input is related to, input can be in indonesian or english, response always in english`
+let othertopic = `Related to : other topic`
+
 let array = [
     {
-        instruction: `Choose from ('weather' or 'other topic') what input is related to`,
+        instruction: instruction,
+        input: `Siapa namamu ?`,
+        response: othertopic
+    },
+    {
+        instruction: instruction,
+        input: `Apakah hari ini akan hujan ?`,
+        response: `Related to : weather, detail : rain, when : today`
+    },
+    {
+        instruction: instruction,
+        input: `Apakah besok akan hujan ?`,
+        response: `Related to : weather, detail : rain, when : tommorow`
+    },
+    {
+        instruction: instruction,
+        input: `Apakah minggu depan akan hujan ?`,
+        response: `Related to : weather, detail : rain, when : next week`
+    },
+    {
+        instruction: instruction,
         input: `Whats the weather like next week ?`,
         response: `Related to : weather, detail : weather, when : next week`
-    },    
+    },
     {
-        instruction: `Choose from ('weather' or 'other topic') what input is related to`,
+        instruction: instruction,
         input: `Whats the weather like today ?`,
         response: `Related to : weather, detail : weather, when : today`
-    },    
+    },
     {
-        instruction: `Choose from ('weather' or 'other topic') what input is related to`,
+        instruction: instruction,
         input: `Will it rain today ?`,
         response: `Related to : weather, detail : rain, when : today`
-    },    
+    },
     {
-        instruction: `Choose from ('weather' or 'other topic') what input is related to`,
+        instruction: instruction,
         input: `Will it rain next week ?`,
         response: `Related to : weather, detail : rain, when : next week`
-    },    
+    },
     {
-        instruction: `Choose from ('weather' or 'other topic') what input is related to`,
+        instruction: instruction,
         input: `Will it snow today ?`,
         response: `Related to : weather, detail : snow, when : today`
-    },    
+    },
     {
-        instruction: `Choose from ('weather' or 'other topic') what input is related to`,
+        instruction: instruction,
         input: `Will it snow tommorow ?`,
         response: `Related to : weather, detail : snow, when : tommorow`
-    },    
+    },
     {
-        instruction: `Choose from ('weather' or 'other topic') what input is related to`,
+        instruction: instruction,
         input: `Is thunderstorm coming ?`,
         response: `Related to : weather, detail : thunderstorm, when : today`
-    },    
+    },
     {
-        instruction: `Choose from ('weather' or 'other topic') what input is related to`,
+        instruction: instruction,
         input: `What's your name ?`,
-        response: `Related to : other topic`
-    },    
+        response: othertopic
+    },
     {
-        instruction: `Choose from ('weather' or 'other topic') what input is related to`,
+        instruction: instruction,
         input: `Tell me about trump`,
-        response: `Related to : other topic`
-    },    
+        response: othertopic
+    },
     {
-        instruction: `Choose from ('weather' or 'other topic') what input is related to`,
+        instruction: instruction,
         input: `Explain why you so smart`,
-        response: `Related to : other topic`
-    },    
+        response: othertopic
+    },
     {
-        instruction: `Choose from ('weather' or 'other topic') what input is related to`,
+        instruction: instruction,
         input: `I want to order pizza`,
-        response: `Related to : other topic`
-    },    
+        response: othertopic
+    },
 ]
 
 fs.writeFile(filename, '', (err) => {
@@ -68,7 +91,7 @@ fs.writeFile(filename, '', (err) => {
 
 array.forEach(e => {
     const text = `### Instruction : ${e.instruction}\n### Input : ${e.input}\n### Response : ${e.response}\n`
-    const json = JSON.stringify({text:text})
+    const json = JSON.stringify({ text: text })
     // console.log(text)
     fs.appendFile(filename, json + '\n', (err) => {
         if (err) {
