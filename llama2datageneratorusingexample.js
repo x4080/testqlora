bf233092cc1f78451aed3e3bd6755ce9ba62fb5f
x4080 <x4080@yahoo.com>
2023-10-03 09:21:10 +0700

    commit1

---
+++ llama2datageneratorusingexample.js
@@ -0,0 +1,67 @@
+const filename = "llama2data.jsonl"
+const fs = require('fs');
+
+let instruction = `Choose from one of ('weather' or 'other topic') for response`
+let example =`# Context : 
+# Input : Whats the weather like next week ?
+# Response : Related to : weather, detail : weather, when : next week, how many days from today : 0
+# Context : 
+# Input : who are trump
+# Response : other topic
+# Context : rain
+# Input : how about next week
+# Response : Related to : weather, detail : rain, when : next week, how many days from today : 7`
+
+// let lines = example.split('\n');
+// let combinedText = lines.join('\n');
+// console.log(combinedText);
+
+
+let array = [
+
+    {
+        instruction: instruction,
+        context: `rain`,
+        input: `how about 2 days from now`,
+        response: 'Related to : weather, detail : rain, when : 2 days from now, how many days from today : 2'
+    },
+    {
+        instruction: instruction,
+        context: `snow`,
+        input: `how about 2 days from now`,
+        response: 'Related to : weather, detail : snow, when : 2 days from now, how many days from today : 2'
+    },
+    {
+        instruction: instruction,
+        context: `thunderstorm`,
+        input: `bagaimana dengan kemarin`,
+        response: 'Related to : weather, detail : thunderstorm, when : yesterday, how many days from today : -1'
+    },
+    {
+        instruction: instruction,
+        context: `snow`,
+        input: `bagaimana dengan kemarin`,
+        response: 'Related to : weather, detail : snow, when : yesterday, how many days from today : -1'
+    },
+]
+
+fs.writeFile(filename, '', (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+});
+
+array.forEach(e => {
+    let text = `# Instruction : ${e.instruction}\nexample\n`
+    text=text+example+'\nsolve this\n'+`# Context : ${e.context}\n# Input : ${e.input}\n# Response : ${e.response}\n`
+    // console.log(text)
+    const json = JSON.stringify({ text: text })
+    // console.log(text)
+    fs.appendFile(filename, json + '\n', (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+    });
+});
